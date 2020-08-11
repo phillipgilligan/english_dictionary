7c0001ab707d51269c720d7bcc0e19a469e1aaa9
Phillip Gilligan <philip.giligan@outlook.com>
2020-08-11 08:51:07 -0500

    Added code to handle proper nouns

--- main.py
+++ main.py
@@ -3,23 +3,25 @@ from difflib import get_close_matches
 
 data = json.load(open("data.json"))
 
-def find_def(word):
+def translate(word):
     word = word.lower()
     if word in data:
         return data[word]
+    elif word.title() in data: #if user entered "texas" this will check for "Texas" as well.
+        return data[word.title()]
     elif len(get_close_matches(word, data.keys())) > 0:
         yn = input("Did you mean %s instead? Enter Y if yes, or N if no: " % get_close_matches(word, data.keys())[0])
-        if yn.lower() == "y":
+        if yn == "Y":
             return data[get_close_matches(word, data.keys())[0]]
-        elif yn.lower() == "n":
+        elif yn == "N":
             return "The word doesn't exist. Please double check it."
         else:
             return "We didn't understand your entry."
     else:
         return "The word doesn't exist. Please double check it."
-
+ 
 word = input("Enter word: ")
-output = find_def(word)
+output = translate(word)
 if type(output) == list:
     for item in output:
         print(item)
