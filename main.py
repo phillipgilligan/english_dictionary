51174d0ab86c3e84210ae2f4dbce7fc0491048d7
Phillip Gilligan <philip.giligan@outlook.com>
2020-08-11 08:54:46 -0500

    Added more checks on words

--- main.py
+++ main.py
@@ -7,8 +7,10 @@ def translate(word):
     word = word.lower()
     if word in data:
         return data[word]
-    elif word.title() in data: #if user entered "texas" this will check for "Texas" as well.
+    elif word.title() in data:
         return data[word.title()]
+    elif word.upper() in data:
+        return data[word.upper()]
     elif len(get_close_matches(word, data.keys())) > 0:
         yn = input("Did you mean %s instead? Enter Y if yes, or N if no: " % get_close_matches(word, data.keys())[0])
         if yn == "Y":
@@ -19,7 +21,7 @@ def translate(word):
             return "We didn't understand your entry."
     else:
         return "The word doesn't exist. Please double check it."
- 
+    
 word = input("Enter word: ")
 output = translate(word)
 if type(output) == list:
