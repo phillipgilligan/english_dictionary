e4b73fe4e2866d0d4ce5c81e5a586f3306c2d1e1
Phillip Gilligan <philip.giligan@outlook.com>
2020-08-11 08:03:33 -0500

    Fixed broken function call and corected typos

--- main.py
+++ main.py
@@ -1,30 +1,34 @@
 import json
 from difflib import get_close_matches
 
+#Importing our json data and saving it to an object
 data = json.load(open("data.json"))
 
+#Function gets posible matches when a user mistypes a word
+def get_matches(user_input):
+    match_word =  get_close_matches(user_input, data.keys())[0]
+    cont_prog(match_word)
 
-def get_matches(usrInput):
-    match =  get_close_matches(usrInput, data.keys())[0]
-    cont_prog(match)
-    
-def cont_prog(match):
-    print (f"Did you mean: '{match}' ?")
-    answer = input("Yes/No: ") 
-    if answer.lower() == "yes":
-        print("Please retype the word.")
-        user_input()
+#Prompts user to continue program when user enters wrong word
+def cont_prog(match_word):
+    print (f"Did you mean: '{match_word}' ?")
+    user_answer = input("Yes/No: ") 
+    if user_answer.lower() == "yes":
+        print (data[match_word])
     else:
         return "The word doesn't exist, please check it or update definitions."
 
-def find_def(usrInput):
-    if usrInput in data:
-        return data[usrInput]
+#Finds definition of the word given
+def find_def(user_input):
+    if user_input in data:
+        print (data[user_input])
     else:
-        get_matches(usrInput)
-        
-def user_input():
-    usrInput = input("Enter word: ")
-    return find_def(usrInput).lower()
+        get_matches(user_input)
 
-print(user_input())
+#Gets user input for find_def      
+def get_word():
+    user_input = input("Enter word: ")
+    user_input = user_input.lower()
+    return find_def(user_input)
+
+print(())
